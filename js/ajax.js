b9df64b618fd4897edff265983e5f4af3ccd3e16
M. Mare <michael.mare@soprasteria.com>
2020-11-23 16:23:02 +0100

    contact fonctionnel

---
+++ js/ajax.js
@@ -0,0 +1,27 @@
+$(function() {
+
+    $('#contact-form').submit(function(e){
+        e.preventDefault();
+        $('.comment-error').empty();
+        var postData = $("#contact-form").serialize();
+
+        $.ajax({
+            type: 'POST',
+            url: 'php/contact.php',
+            data: postData,
+            dataType: 'json',
+            success: function(result){
+
+                if(result.isSuccess){
+                    $("#contact-form").append("<p class='thank-you'>Votre message a bien été envoyé. Merci de m'avoir contacté</p>");
+                    $("#contact-form")[0].reset();
+                } else {
+                    $("#name + .comment-error").html(result.nameError);
+                    $("#city + .comment-error").html(result.cityError);
+                    $("#email + .comment-error").html(result.emailError);
+                    $("#message + .comment-error").html(result.messageError);
+                }
+            }
+        })
+    });
+})
---
+++ php/contact.php
@@ -0,0 +1,70 @@
+<?php 
+    $array = array("name" => "", "city" => "", "email" => "", "message" => "", "nameError" => "", "cityError" => "", "emailError" => "", "messageError" => "", "isSuccess" => "false");
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (isset($_POST['name'])) {
+            $array["name"] = verifyInput($_POST['name']);
+        }
+        if (isset($_POST['city'])) {
+            $array["city"] = verifyInput($_POST['city']);
+        }
+        if (isset($_POST['email'])) {
+            $array["email"] = verifyInput($_POST['email']);
+        }
+        if (isset($_POST['message'])) {
+            $array["message"] = verifyInput( $_POST['message']);
+        }
+        $array["isSuccess"] = true;
+        $emailText = "";
+
+        if(empty($array["name"])){
+            $array["nameError"] = "Le nom n'a pas été spécifié";
+            $array["isSuccess"] = false;
+        } else {
+            $emailText .= "Name: {$array['name']}\n";
+        }
+
+        if(empty($array["city"])){
+            $array["cityError"] = "La ville n'a pas été spécifiée";
+            $array["isSuccess"] = false;
+        } else {
+            $emailText .= "City: {$array['city']}\n";
+        }
+        if(!isEmail($array["email"])){
+            $array["emailError"] = "Il ne s'agit pas d'un email correct";
+            $array["isSuccess"] = false;
+        } else {
+            $emailText .= "Email: {$array['email']}\n";
+        }
+
+        if(empty($array["message"])){
+            $array["messageError"] = "Oups ! Le champ du message est vide";
+            $array["isSuccess"] = false;
+        } else {
+            $emailText .= "Message: {$array['message']}\n";
+        }
+
+
+        if($array["isSuccess"]) {
+            $emailTo = "wawamika@gmail.com";
+            $headers = "From: {$array['name']} <{$array['email']}>\r\nReply-To: {$array['email']}";
+            mail($emailTo, "Un message de votre site", $emailText, $headers);
+        }
+
+        echo json_encode($array);
+
+    };
+
+    function isEmail($var) {
+        return filter_var($var, FILTER_VALIDATE_EMAIL);
+    };
+
+    function verifyInput($var) {
+        $var = trim($var);
+        $var = stripslashes($var);
+        $var = htmlspecialchars($var);
+
+        return $var;
+    };
+
+?>
